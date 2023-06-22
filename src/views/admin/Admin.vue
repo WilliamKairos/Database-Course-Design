ce868960809272ca2ff1acd47cb18699f381c8e7
WilliamKairos <wwilliamkairos@gmaill.com>
2023-06-22 22:08:19 +0800

    6.22

--- src/views/admin/Admin.vue
+++ src/views/admin/Admin.vue
@@ -2,28 +2,15 @@
   <div>
     <div class="header">高校奖金评定管理系统</div>
     <div class="main">
-      <el-menu class="sidebar">
-        <el-menu-item>
-          <router-link to="/admin">管理面板</router-link>
-        </el-menu-item>
-        <el-menu-item>
-          <router-link to="/admin/student-management">学生管理</router-link>
-        </el-menu-item>
-        <el-menu-item>
-          <router-link to="/admin/scholarship-types">奖学金种类管理</router-link>
-        </el-menu-item>
-        <el-menu-item>
-          <router-link to="/admin/scholarship-applications">奖学金申请管理</router-link>
-        </el-menu-item>
-        <el-menu-item>
-          <router-link to="/admin/evaluation-results">评定结果管理</router-link>
-        </el-menu-item>
-        <el-menu-item>
-          <router-link to="/admin/admin-management">管理员管理系统管理</router-link>
-        </el-menu-item>
+      <el-menu class="sidebar" router>
+        <el-menu-item index="/admin/student-management">学生管理</el-menu-item>
+        <el-menu-item index="/admin/scholarship-type-management">奖学金种类管理</el-menu-item>
+        <el-menu-item index="/admin/scholarship-application-management">奖学金申请管理</el-menu-item>
+        <el-menu-item index="/admin/evaluation-result-management">评定结果管理</el-menu-item>
+        <el-menu-item index="/admin/admin-management">管理员管理系统管理</el-menu-item>
       </el-menu>
       <div class="content">
-        <router-view></router-view> <!-- 这里将显示子路由的内容 -->
+        <router-view></router-view>
       </div>
     </div>
   </div>
