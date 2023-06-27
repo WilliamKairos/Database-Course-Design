21f076dac487c064c2cdb7118afac485516dd6ed
williamkairos <wwilliamkairos@gmail.com>
2023-06-27 21:26:27 +0800

    6.27

--- Backend/src/main/java/com/scholarship/controller/UserController.java
+++ Backend/src/main/java/com/scholarship/controller/UserController.java
@@ -3,6 +3,7 @@ import com.scholarship.entity.User;
 import com.scholarship.service.UserService;
 import com.scholarship.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,16 +21,33 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public Result<User> login(@RequestBody Map<String, String> requestData) {
+    public ResponseEntity<Result<User>> login(@RequestBody Map<String, String> requestData) {
         String username = requestData.get("username");
         String password = requestData.get("password");
+        String userType = requestData.get("userType"); // 接收用户身份类型
 
+        System.out.println(userType);
         User user = userService.findUserByUsernameAndPassword(username, password);
+        System.out.println(userType);
+        System.out.println(user.getUserType());
         System.out.println(user);
-        if ( user == null) {
-            return new Result<>(401, "登录失败，用户名或密码不正确", null);
-        }else {
-            return new Result<>(200, "登录成功!", user);
+        if (user == null) {
+            Result<User> result = new Result<>(201, "登录失败，用户名或密码不正确", null);
+            return new ResponseEntity<>(result, HttpStatus.UNAUTHORIZED);
+        } else {
+            if (!user.getUserType().equals(userType)) { // 验证用户身份类型
+                Result<User> result = new Result<>(201, "登录失败，身份类型不匹配", user);
+                return new ResponseEntity<>(result, HttpStatus.UNAUTHORIZED);
+            }
+            Result<User> result = new Result<>(200, "登录成功!", user);
+            return new ResponseEntity<>(result, HttpStatus.OK);
         }
+
+//
+//        else {
+//            Result<User> result = new Result<>(200, "登录成功!", user);
+//            return new ResponseEntity<>(result, HttpStatus.OK);
+//        }
     }
+
 }
--- Backend/src/main/java/com/scholarship/entity/User.java
+++ Backend/src/main/java/com/scholarship/entity/User.java
@@ -2,14 +2,23 @@ package com.scholarship.entity;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+
 @Data
 public class User {
     private Integer id;
     private String username;
     private String password;
+    @Column(name = "user_type")
     private String userType;
+
+//    private String userType;
 
     public User() {
         // 默认构造函数
     }
+//    public String getUserType() {
+//        // 返回用户的身份类型，确保返回非空的字符串
+//        return userType;
+//    }
 }
